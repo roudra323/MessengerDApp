dc40792098c33a4d30330571a21e77850a08f84b
roudra323 <asirroudra@gmail.com>
2023-08-04 03:13:20 +0600

    added couple of tests

--- test/Messenger.js
+++ test/Messenger.js
@@ -5,32 +5,47 @@ describe("Messenger", function () {
   let owner;
   let friend1;
   let friend2;
+  let contract;
 
-  //   beforeEach(async function () {
-  //     [owner, friend1, friend2] = await ethers.getSigners();
-  //     console.log("Deploying contracts with the account:", owner.address);
-  //     const contract = await ethers.deployContract("Messenger");
-  //     console.log("Contract address:", await contract.getAddress());
-  //   });
+  beforeEach(async function () {
+    [owner, friend1, friend2] = await ethers.getSigners();
+    contract = await ethers.deployContract("Messenger");
+  });
 
   describe("Create account", function () {
     it("should create an account and emit NewMember event", async function () {
-      [owner, friend1, friend2] = await ethers.getSigners();
-      console.log("Deploying contracts with the account:", owner.address);
-      const contract = await ethers.deployContract("Messenger");
-      console.log("Contract address:", await contract.getAddress());
-
       const name = "Alice";
       await contract.createAcc(name);
-
       const user = await contract.userList(owner.address);
       expect(user).to.equal(name);
-
-      const uname = await contract.checkUserExists(owner.address);
-      console.log("This is user information", uname);
-      expect(uname).to.equal(true);
+    });
+    it("Should return true if address is Registered", async function () {
+      await contract.createAcc("Alex");
+      const isRegi = await contract.checkUserExists(owner.address);
+      expect(isRegi).to.equal(true);
+    });
+    it("Should emit NewMember event", async function () {
+      await expect(contract.createAcc("Alex"))
+        .to.emit(contract, "NewMember")
+        .withArgs(owner.address, "Alex");
     });
   });
-
   // Add more test cases to cover other functionalities of the contract
+  describe("Send Friend Request", function () {
+    it("Should sent friend rerquest", async function () {
+      await contract.connect(friend1).createAcc("Alex");
+      await contract.connect(friend2).createAcc("Bob");
+      await contract.connect(friend1).sendRequest(friend2.address);
+      const array = await contract.connect(friend1).getAllSentRequest();
+      expect(array.length).to.equal(1);
+    });
+
+    it("Should receive friend rerquest", async function () {
+      await contract.connect(friend1).createAcc("Alex");
+      await contract.connect(friend2).createAcc("Bob");
+      await contract.connect(friend1).sendRequest(friend2.address);
+      const array = await contract.connect(friend2).getAllReceivedRequest();
+      expect(array.length).to.equal(1);
+    });
+  });
 });
