91843df31fcb89777da4f86401a181bec4ca9d3a
roudra323 <asirroudra@gmail.com>
2023-08-24 00:52:52 +0600

    added tailwind

--- src/components/Chatting/Chat.jsx
+++ src/components/Chatting/Chat.jsx
@@ -4,9 +4,7 @@ import Sidebar from "../Sidebar/Sidebar";
 function Chat() {
   return (
     <div className="page-container">
-      <div className="content-container">
-        <Sidebar />
-      </div>
+      <div className="content-container">{/* <Sidebar /> */}</div>
     </div>
   );
 }
