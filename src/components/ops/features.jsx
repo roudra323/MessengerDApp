39d58f1003ee444bd931f76ad332e0ccd07b8ebf
roudra323 <asirroudra@gmail.com>
2023-08-26 02:29:11 +0600

    Added request button

--- src/components/ops/features.jsx
+++ src/components/ops/features.jsx
@@ -1,4 +1,4 @@
-import { Button, Text, Stack } from "@chakra-ui/react";
+import { Button, Text, Stack, VStack } from "@chakra-ui/react";
 import React from "react";
 import Profile from "../profile/Profile";
 import { useNavigate } from "react-router-dom";
@@ -10,16 +10,28 @@ function Features({ state, address }) {
   const { contract } = state;
 
   return (
-    <Stack spacing={4} direction={["column", "row"]} align="center">
-      <Profile address={address} />
-      <Button className="button" onClick={() => navigate("/chat")}>
+    <VStack>
+      <Stack
+        spacing={4}
+        direction={["column", "row"]}
+        align="center"
+        pb={"10px"}
+      >
+        <Profile address={address} />
+        <Button className="button" onClick={() => navigate("/chat")}>
+          <Text pl="10px" color="white" fontSize="xl" fontWeight="extrabold">
+            Chat
+          </Text>
+        </Button>
+        <Friends />
+        <FindFriends />
+      </Stack>
+      <Button className="button">
         <Text pl="10px" color="white" fontSize="xl" fontWeight="extrabold">
-          Chat
+          Received Requests
         </Text>
       </Button>
-      <Friends />
-      <FindFriends />
-    </Stack>
+    </VStack>
   );
 }
 
